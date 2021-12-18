3c925cc0eedaf2b5efa931e067e9c5ec45afd2ae
Denis Davydov <denis.davydov.personal.email@gmail.com>
2021-12-18 20:17:40 +0300

    project init

---
+++ spring-data-jpa-experiments/src/main/java/com/dobraccoon/springdatajpaexperiments/SpringDataJpaExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.dobraccoon.springdatajpaexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaExperimentsApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/SpringDataJpaExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobraccoon.springdatajpaexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
