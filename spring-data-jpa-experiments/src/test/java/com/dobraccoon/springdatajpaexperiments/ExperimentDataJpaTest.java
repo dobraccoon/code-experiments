8dd652652298ac95d625469830c32d6c5d9ac304
Denis Davydov <denis.davydov.personal.email@gmail.com>
2021-12-19 16:09:36 +0300

    Experiment: If use @DataJpaTest Spring make up only needed context unlike @SpringBootTest and run tests on H2 database, not on that database that is configured into project

---
+++ spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/ExperimentDataJpaTest.java
@@ -0,0 +1,29 @@
+package com.dobraccoon.springdatajpaexperiments;
+
+import com.dobraccoon.springdatajpaexperiments.books.Book;
+import com.dobraccoon.springdatajpaexperiments.books.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class ExperimentDataJpaTest {
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Test
+	void BookRepositoryTest() {
+		bookRepository.save(new Book(""));
+		long count = bookRepository.count();
+
+		assertThat(count).isEqualTo(1);
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
