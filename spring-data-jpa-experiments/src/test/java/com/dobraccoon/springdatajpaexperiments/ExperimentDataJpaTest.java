d512f6ba1f6d250909553a51bcf13516828b93f2
Denis Davydov <denis.davydov.personal.email@gmail.com>
2021-12-19 17:59:47 +0300

    Experiment: If you use @DataJpaTest, each test method rollback all changes, that it make. That's why you mustn't use data from another test in your test, or use order and @Rollback(value = false), bu it's not recommended

--- spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/ExperimentDataJpaTest.java
+++ spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/ExperimentDataJpaTest.java
@@ -2,24 +2,36 @@ package com.dobraccoon.springdatajpaexperiments;
 
 import com.dobraccoon.springdatajpaexperiments.books.Book;
 import com.dobraccoon.springdatajpaexperiments.books.BookRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 class ExperimentDataJpaTest {
 
 	@Autowired
 	private BookRepository bookRepository;
 
+	@Rollback(value = false)
+	@Order(1)
 	@Test
-	void BookRepositoryTest() {
+	void BookRepositoryTest1() {
 		bookRepository.save(new Book(""));
 		long count = bookRepository.count();
 
 		assertThat(count).isEqualTo(1);
+		//After method end all transaction into method will rollback
+	}
+
+	@Order(2)
+	@Test
+	void BookRepositoryTest2() {
+		//That's why this check will fail without @Rollback in previous test
+		assertThat(bookRepository.count()).isEqualTo(1);
 	}
 
 	@Test
