69d0653bca1d2ac1314874ed250a747fb437511e
Denis Davydov <denis.davydov.personal.email@gmail.com>
2021-12-19 17:45:30 +0300

    commented DB connection and removed bad test

--- spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/SpringDataJpaExperimentsApplicationTests.java
+++ spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/SpringDataJpaExperimentsApplicationTests.java
@@ -15,19 +15,6 @@ class SpringDataJpaExperimentsApplicationTests {
 	private BookRepository bookRepository;
 
 	@Test
-	void bookRepositoryTest() {
-		bookRepository.save(new Book(""));
-		bookRepository.save(new Book(""));
-		bookRepository.save(new Book(""));
-
-		/*
-		If you run this test twice
-		it would be problem because created objects didn't was removed
-		 */
-		assertThat(bookRepository.count()).isEqualTo(3);
-	}
-
-	@Test
 	void contextLoads() {
 	}
 
