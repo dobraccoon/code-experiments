5206bf07739fcf782e6be7d431f5709a195daf22
Denis Davydov <denis.davydov.personal.email@gmail.com>
2021-12-19 17:44:05 +0300

    Experiment: If use @SpringBootTest it use that DB that is described into properties, that's why either use it and remove objects, that you create, either use DataJpaTest, that use H2 DB and not run all app context

--- spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/SpringDataJpaExperimentsApplicationTests.java
+++ spring-data-jpa-experiments/src/test/java/com/dobraccoon/springdatajpaexperiments/SpringDataJpaExperimentsApplicationTests.java
@@ -1,10 +1,31 @@
 package com.dobraccoon.springdatajpaexperiments;
 
+import com.dobraccoon.springdatajpaexperiments.books.Book;
+import com.dobraccoon.springdatajpaexperiments.books.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SpringDataJpaExperimentsApplicationTests {
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Test
+	void bookRepositoryTest() {
+		bookRepository.save(new Book(""));
+		bookRepository.save(new Book(""));
+		bookRepository.save(new Book(""));
+
+		/*
+		If you run this test twice
+		it would be problem because created objects didn't was removed
+		 */
+		assertThat(bookRepository.count()).isEqualTo(3);
+	}
 
 	@Test
 	void contextLoads() {
